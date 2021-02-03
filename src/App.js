ff01f3e1fa4a2de0787fbeb278a171f528fa551c
Alvinkariuki <alvinkariuki230@gmail.com>
2021-02-04 00:11:51 +0300

    Complete functionality, future works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from "words-to-numbers";
+
+// Material UI imports
+import useStyles from "./styles";
 
 // Component imports
 import NewsCards from "./components/NewsCards/NewsCards";
@@ -8,15 +12,34 @@ const alanKey =
   "ebb7fd9b11f8a5c7d53529ef63a71d902e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
 
   // Load button
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Kindly repeat that word");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening article");
+          }
         }
       },
     });
@@ -24,8 +47,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>Allan AI news application</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        <img
+          src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg"
+          className={classes.alanLogo}
+          alt="alan logo"
+        />
+      </div>
+
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 };
