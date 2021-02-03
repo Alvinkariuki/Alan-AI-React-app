f5fb1008eaeb2e3e094743a2bbb40d9bd6399eae
Alvinkariuki <alvinkariuki230@gmail.com>
2021-02-03 19:16:16 +0300

    Card setup and API consumed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+
+// Component imports
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "ebb7fd9b11f8a5c7d53529ef63a71d902e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   // Load button
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === "testCommand") {
-          alert("Code was executed");
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
         }
       },
     });
@@ -20,6 +25,7 @@ const App = () => {
   return (
     <div>
       <h1>Allan AI news application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
